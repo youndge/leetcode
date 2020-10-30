21750f6e801977014a6738b234ca006c76924ac5
马靖玮 <majingwei@majingweideMBP.lan>
2020-10-30 23:01:13 +0800

    go 463

--- GO/463/463.go
+++ GO/463/463.go
@@ -2,11 +2,41 @@ package main
 
 import "fmt"
 
-/*1.自己写的方法
+/*1.自己写的方法*/
+/*
+执行用时：60 ms, 在所有 Go 提交中击败了97.73%的用户
+内存消耗：7 MB, 在所有 Go 提交中击败了42.37%的用户
+*/
+func islandPerimeter(grid [][]int) int {
+	sum := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if 1 != grid[i][j] {
+				continue
+			}
+			sum += 4
+			if i >= 1 && 1 == grid[i-1][j] {
+				sum--
+			}
+			if i < len(grid)-1 && 1 == grid[i+1][j] {
+				sum--
+			}
+			if j >= 1 && 1 == grid[i][j-1] {
+				sum--
+			}
+			if j < len(grid[i])-1 && 1 == grid[i][j+1] {
+				sum--
+			}
+		}
+	}
+	return sum
+}
+
+/*
 执行用时：96 ms, 在所有 Go 提交中击败了12.88%的用户
 内存消耗：7 MB, 在所有 Go 提交中击败了19.49%的用户
 */
-func islandPerimeter(grid [][]int) int {
+func islandPerimeter2(grid [][]int) int {
 	sum := 0
 	for i := range grid {
 		for j := range grid[i] {
@@ -38,35 +68,35 @@ func islandPerimeter(grid [][]int) int {
 执行用时：72 ms, 在所有 Go 提交中击败了83.33%的用户
 内存消耗：7.6 MB, 在所有 Go 提交中击败了11.02%的用户
 */
-type pair struct{ x, y int }
+// type pair struct{ x, y int }
 
-var dir4 = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+// var dir4 = []pair{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
 
-func islandPerimeter2(grid [][]int) (ans int) {
-	n, m := len(grid), len(grid[0])
-	var dfs func(x, y int)
-	dfs = func(x, y int) {
-		if x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0 {
-			ans++
-			return
-		}
-		if grid[x][y] == 2 {
-			return
-		}
-		grid[x][y] = 2
-		for _, d := range dir4 {
-			dfs(x+d.x, y+d.y)
-		}
-	}
-	for i, row := range grid {
-		for j, v := range row {
-			if v == 1 {
-				dfs(i, j)
-			}
-		}
-	}
-	return
-}
+// func islandPerimeter2(grid [][]int) (ans int) {
+// 	n, m := len(grid), len(grid[0])
+// 	var dfs func(x, y int)
+// 	dfs = func(x, y int) {
+// 		if x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0 {
+// 			ans++
+// 			return
+// 		}
+// 		if grid[x][y] == 2 {
+// 			return
+// 		}
+// 		grid[x][y] = 2
+// 		for _, d := range dir4 {
+// 			dfs(x+d.x, y+d.y)
+// 		}
+// 	}
+// 	for i, row := range grid {
+// 		for j, v := range row {
+// 			if v == 1 {
+// 				dfs(i, j)
+// 			}
+// 		}
+// 	}
+// 	return
+// }
 
 /*3.迭代*/
 /*
