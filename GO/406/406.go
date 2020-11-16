a6c8c2c9754fc28f3467c8daf4c1d2c425d29dc5
马靖玮 <majingwei@majingweideMBP.lan>
2020-11-16 22:54:55 +0800

    go 406

---
+++ GO/406/406.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func reconstructQueue(people [][]int) [][]int {
+	ans := [][]int{}
+	sort.Slice(people, func(i, j int) bool {
+		a, b := people[i], people[j]
+		return a[0] > b[0] || a[0] == b[0] && a[1] < b[1]
+	})
+	for _, person := range people {
+		idx := person[1]
+		ans = append(ans[:idx], append([][]int{person}, ans[idx:]...)...)
+	}
+	return ans
+}
+func main() {
+	people := [][]int{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}}
+	fmt.Println(reconstructQueue(people))
+}
