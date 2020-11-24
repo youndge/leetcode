3a753c20b20d635bc25ed45f7f7b7f668a46eacd
马靖玮 <majingwei@majingweideMBP.lan>
2020-11-24 22:59:11 +0800

    go 222

---
+++ GO/222/222.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	level := 0
+	for node := root; node.Left != nil; node = node.Left {
+		level++
+	}
+	return sort.Search(1<<(level+1), func(k int) bool {
+		if k <= 1<<level {
+			return false
+		}
+		bits := 1 << (level - 1)
+		node := root
+		for node != nil && bits > 0 {
+			if bits&k == 0 {
+				node = node.Left
+			} else {
+				node = node.Right
+			}
+			bits >>= 1
+		}
+		return node == nil
+	}) - 1
+}
+
+func main() {
+	n1 := &TreeNode{Val: 1}
+	n2 := &TreeNode{Val: 2}
+	n3 := &TreeNode{Val: 3}
+	n4 := &TreeNode{Val: 4}
+	n5 := &TreeNode{Val: 5}
+	n6 := &TreeNode{Val: 6}
+	n1.Left, n1.Right = n2, n3
+	n2.Left, n2.Right = n4, n5
+	n3.Left = n6
+	fmt.Println(countNodes(n1))
+}
